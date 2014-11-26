eeeb45fc8cb0980538526ea906996ebc8206be82
Leon Weidauer <leon@lnwdr.de>
2014-11-26 10:07:33 +0100

    small corrections and a challenge at the end ;)

--- data_comparison.js
+++ data_comparison.js
@@ -5,7 +5,7 @@ function deepEqual(object, object1) {
     var obj;
     var obj1;
     if (typeof object !== "object" || typeof object1 !== "object" ||
-            object == null || object1 == null) {
+            object === null || object1 === null) {
                 return false;
             }
     // in this case it was better to have negative if conditions, and return false each time they are met.
@@ -24,20 +24,20 @@ function deepEqual(object, object1) {
     }
     if (count !== count1) {
         return false;
-    } 
+    }
     // If the number of properties is not the same, return false. If it is, continue to compare the values.
     for (obj in object) {
         if (!object1.hasOwnProperty(obj)) {
             return false;
         }
-        // if the properties of object1 are not named the same as the properties in object (represented by the variable obj), return false. 
+        // if the properties of object1 are not named the same as the properties in object (represented by the variable obj), return false.
 
-        //  if (typeof object[obj] === "object" && typeof object1[obj] === "object") {
-        //  the above code could perform the same function, but it actually does the same as the block in the beginning of the function.
-        //  This justified a recursive call to the function with the object properties (object[obj]) as arguments.
+        // if (typeof object[obj] === "object" && typeof object1[obj] === "object") {
+        // the above code could perform the same function, but it actually does the same as the block in the beginning of the function.
+        // This justified a recursive call to the function with the object properties (object[obj]) as arguments.
 
         if (!deepEqual(object[obj], object1[obj])) {
-            if (object[obj]	!== object1[obj]) {
+            if (object[obj] !== object1[obj]) {
                 return false;
             }
         }
@@ -47,8 +47,8 @@ function deepEqual(object, object1) {
         // - not both object properties are object themselves
         // - they are both objects, but might not have the same number of properties
         // - they do have the same number of properties but not the same property names
-        //  - they have the same property name but not the same values (this already requires a second recursion)
-        //  - if none is false, return true
+        // - they have the same property name but not the same values (this already requires a second recursion)
+        // - if none is false, return true
     return true;
 }
 
@@ -60,3 +60,5 @@ console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
+console.log(deepEqual({a: 0/0}, {a: 0/0}));
+// → false, should be true. This is going to be hard to fix ;)

bd9418229e5e7d383f3dc77930665437202a5e55
Noa Rave <noa.rave@berlinonline.de>
2015-01-12 16:22:47 +0100

    DOM table exercise comments

--- table_html_js.js
+++ table_html_js.js
@@ -6,20 +6,36 @@
 </style>
 
 <script>
+/*
+ * first part of the function creates
+ * an array of properties,
+ * assignes the table and the table row elements
+ * and appends the together.
+ */
   function buildTable(data) {
     var headers = Object.keys(data[0]);
     var table = document.createElement('table');
     var tr = document.createElement('tr');  
     table.appendChild(tr);
-
+/*
+ * This loop takes the headers array
+ * and loops on it. it fills the cells with
+ * the header text and appends it to the
+ * table structure
+ */
     headers.forEach(function(h) {
       var th = document.createElement('th');
       th.textContent =h;
       tr.appendChild(th);
     });
-    
-	console.log(data);
-    
+
+    /*
+     * This loop first creates an element for each row
+     * of the table and then loops over each object element
+     * to fill each cell with text. 
+     * The entered text is set using
+     * current_data_row[current_header]
+     */
     data.forEach(function(d) { 
       var tr = document.createElement('tr');
       table.appendChild(tr);

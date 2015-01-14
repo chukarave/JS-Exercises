32c86342e436f0a15fe07284c2b685f0cb377bc8
Noa Rave <noa.rave@berlinonline.de>
2015-01-14 10:55:48 +0100

    indentation

--- table_html_js.js
+++ table_html_js.js
@@ -1,8 +1,8 @@
 <style>
-  /* Defines a cleaner look for tables */
-  table  { border-collapse: collapse; }
-  td, th { border: 1px solid black; padding: 3px 8px; }
-  th     { text-align: left; }
+/* Defines a cleaner look for tables */
+table  { border-collapse: collapse; }
+td, th { border: 1px solid black; padding: 3px 8px; }
+th     { text-align: left; }
 </style>
 
 <script>
@@ -12,21 +12,21 @@
  * assignes the table and the table row elements
  * and appends the together.
  */
-  function buildTable(data) {
+function buildTable(data) {
     var headers = Object.keys(data[0]);
     var table = document.createElement('table');
     var tr = document.createElement('tr');  
     table.appendChild(tr);
-/*
- * This loop takes the headers array
- * and loops on it. it fills the cells with
- * the header text and appends it to the
- * table structure
- */
+    /*
+     * This loop takes the headers array
+     * and loops on it. it fills the cells with
+     * the header text and appends it to the
+     * table structure
+     */
     headers.forEach(function(h) {
-      var th = document.createElement('th');
-      th.textContent =h;
-      tr.appendChild(th);
+        var th = document.createElement('th');
+        th.textContent =h;
+        tr.appendChild(th);
     });
 
     /*
@@ -37,17 +37,17 @@
      * current_data_row[current_header]
      */
     data.forEach(function(d) { 
-      var tr = document.createElement('tr');
-      table.appendChild(tr);
-      headers.forEach(function(h) { 
-        var td = document.createElement('td');
-        td.textContent = d[h];
-        tr.appendChild(td);
-      });
+        var tr = document.createElement('tr');
+        table.appendChild(tr);
+        headers.forEach(function(h) { 
+            var td = document.createElement('td');
+            td.textContent = d[h];
+            tr.appendChild(td);
+        });
     });
 
     return table;
-  };
+};
 
-  document.body.appendChild(buildTable(MOUNTAINS));
+document.body.appendChild(buildTable(MOUNTAINS));
 </script>
